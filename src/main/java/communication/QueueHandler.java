7dea94242ea94bcbd4dba30e6be285c3dd68090f
Jules Kreutzer <juleskreutzer@me.com>
2018-05-08 13:35:14 +0200

    Commit before branch switch

--- src/main/java/communication/QueueHandler.java
+++ src/main/java/communication/QueueHandler.java
@@ -1,47 +1,25 @@
 package communication;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.pts62.common.finland.communication.IQueueSubscribeCallback;
-import io.sentry.Sentry;
-import service.InvoiceService;
+
+
+import com.pts62.common.finland.communication.QueueConnector;
+import com.pts62.common.finland.communication.QueueConstants;
 
 import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
 import javax.ejb.Startup;
 
 @Startup
-public class QueueHandler  implements IQueueSubscribeCallback{
+public class QueueHandler  {
 
-    @EJB
-    InvoiceService invoiceService;
+    private QueueConnector connector;
 
     @PostConstruct
-    public void setupCommunicationQueue() {
-        new QueueHandler(com.pts62.common.finland.communication.QueueConstants.getAntaMinenQueue());
+    private void setup() {
+        connector = new QueueConnector();
+
     }
 
-    public QueueHandler(String queue) {
-        new com.pts62.common.finland.communication.QueueConnector(queue);
-    }
-
-    @Override
-    public void onMessageReceived(Object o) {
-        try {
-            JsonNode data = (JsonNode) o;
-
-            if(data.has("message") && data.get("message").has("invoice") && data.get("message").get("invoice").has("generate")) {
-                boolean startInvoiceGeneration = data.get("message").get("invoice").get("generate").asBoolean();
-
-                if(startInvoiceGeneration) {
-                    invoiceService.generateInvoices();
-                }
-            } else {
-                throw new Exception("Unable to parse received message");
-            }
-        } catch (Exception e) {
-            Sentry.capture(e);
-            Sentry.getStoredClient().addExtra("MessageQueueMessage", o);
-        }
+    public void test() {
 
     }
 }
