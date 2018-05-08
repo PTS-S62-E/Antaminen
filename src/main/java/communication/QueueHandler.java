b366ff9f00a049035ba6bab58fe9e4ff458da16c
Jules Kreutzer <juleskreutzer@me.com>
2018-05-08 21:17:45 +0200

    Possible to generate invoices using a request from a message queque. See corresponding documentation in Endpoints folder on drive

--- src/main/java/communication/QueueHandler.java
+++ src/main/java/communication/QueueHandler.java
@@ -2,14 +2,29 @@ package communication;
 
 
 
+import com.pts62.common.finland.communication.CommunicationBuilder;
+import com.pts62.common.finland.communication.IQueueSubscribeCallback;
 import com.pts62.common.finland.communication.QueueConnector;
 import com.pts62.common.finland.communication.QueueConstants;
+import exceptions.InvoiceException;
+import io.sentry.Sentry;
+import io.sentry.event.Breadcrumb;
+import io.sentry.event.BreadcrumbBuilder;
+import service.InvoiceService;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.jms.QueueConnectionFactory;
+import java.util.logging.Logger;
 
 @Startup
+@Singleton
 public class QueueHandler  {
+
+    @EJB
+    InvoiceService invoiceService;
 
     private QueueConnector connector;
 
@@ -17,9 +32,38 @@ public class QueueHandler  {
     private void setup() {
         connector = new QueueConnector();
 
+        this.handleRead();
     }
 
-    public void test() {
+    private void handleRead() {
+        CommunicationBuilder invoiceBuilder = new CommunicationBuilder();
+        invoiceBuilder.setCountry("fi");
+        invoiceBuilder.setApplication("antaminen");
+        invoiceBuilder.setMessage("generate.invoices");
 
+        CommunicationBuilder comBuilder = new CommunicationBuilder();
+        comBuilder.setCountry("fi");
+        comBuilder.setApplication("antaminen");
+        comBuilder.setMessage("*");
+
+        connector.readMessage(invoiceBuilder.build(), new IQueueSubscribeCallback() {
+            @Override
+            public void onMessageReceived(String s) {
+                try {
+                    invoiceService.generateInvoices();
+                } catch (InvoiceException e) {
+                    Sentry.capture(e);
+                }
+            }
+        });
+
+        connector.readMessage(comBuilder.build(), new IQueueSubscribeCallback() {
+            @Override
+            public void onMessageReceived(String s) {
+                Exception e = new Exception("Unable to process MQ request for specific route with data " + s);
+                Sentry.getContext().recordBreadcrumb( new BreadcrumbBuilder().setMessage(s).build());
+                Sentry.capture(e);
+            }
+        });
     }
 }
